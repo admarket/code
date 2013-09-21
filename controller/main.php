e2173a5a5b31e76d515eac1f08881d63099f62d6
liu <liuyongpo@gmail.com>
2013-09-21 11:04:41 +0800

    变量名fix

--- controller/main.php
+++ controller/main.php
@@ -43,8 +43,8 @@ class main extends spController
     function email() {
                 $mail = spClass('spEmail');
                 $email=$this->spArgs("email"); // 用spArgs接收spUrl传过来的email
-                $email=$this->encryptEmail($email);
-                $address="http://www.eadmarket.com/active_member.html?ticket=".$email;
+                $emailx=$this->encryptEmail($email);
+                $address="http://www.eadmarket.com/active_member.html?ticket=".$emailx;
                 $addition="<p>此邮件为系统自动发送的邮件，请勿直接回复</p>";
                 $mailsubject = "广告市场注册验证邮箱";//邮件主题
                 $mailbody = "<h4> 请点击下面验证地址进行验证：</h4>"."<p> <a href=".$address.">".$address."<a></p>".$addition;//邮件内容
