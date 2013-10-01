502162b4fbda68f29f7e3a89e36b14e37954970f
admarket <4006776@qq.com>
2013-10-01 16:18:22 +0800

    修正部分显示bug

--- tpl/user/inner-head.php
+++ tpl/user/inner-head.php
@@ -4,7 +4,7 @@
            <div class="span2">
             <a href="/" title="网站首页"><img class="logo-small" src="/img/logo-small.png"/></a>
           </div>
-          <div class="span2 offset1" style="padding:5px;">
+          <div class="span2 offset1" style="padding-top:5px;">
               <{if $smarty.session.user.type==1}>
                   <a  class="btn btn-mini btn-primary tip"  data-placement="bottom" title="点击切换为买家"    href="<{spUrl c=cuser a=changeIdentity}>">
                     <i class="icon-user"></i>&nbsp;卖家
@@ -15,7 +15,7 @@
                 </a>
                <{/if}>
           </div>
-          <div class="span6">
+          <div class="span6  offset1">
               <ul class="nav nav-pills nav-head">
                 <{if $smarty.session.user.type==1}>
                   <li <{if $current=="sitemanage"}>class="active"<{/if}> ><a href="<{spUrl c=sub a=sitemanage}>">网站管理</a></li>
